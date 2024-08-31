c3d4d838b7ad6487db8bd5e6d77a41157c67eeb3
shaungpaung <shaungpaung2014@gmail.com>
2024-08-31 21:04:54 +0630

    just changes

--- routes/api.php
+++ routes/api.php
@@ -22,11 +22,10 @@ Route::get('/users', function (Request $request) {
 
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/login', [UserController::class, 'login']);
     Route::post('/logout', [UserController::class, 'logout']);
     Route::post('change_password', [UserController::class, 'changePassword']);
     Route::post('reset_password', [UserController::class, 'resetPassword']);
     Route::apiResource('/vocabularies', VocabularyController::class);
 });
-
-Route::post('/login', [UserController::class, 'login']);
 Route::apiResource('/users', UserController::class);
